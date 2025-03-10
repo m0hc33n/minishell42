dacd601afe99c9fa8d082cddea37a8de8f2b0770
Mouhcine Amaziane <mamazian@e2r2p4.1337.ma>
2025-03-10 17:06:53 +0000

    Fix: Address Invalid Access

--- src/lexer/spaceit.c
+++ src/lexer/spaceit.c
@@ -2,25 +2,29 @@
 
 static t_status	cmd_spaced_len(char *cmdline, uint64_t *sz)
 {
+	char	prev_char;
+
+	prev_char = 0;
 	while (*cmdline)
 	{
 		if (minishell_iscmdsep(*cmdline))
 		{
-			if (!minishell_iscmdsep(*(cmdline - 1))) //  fix this -1 shit
+			if (!minishell_iscmdsep(prev_char))
 				(*sz)++;
-			else if (*cmdline != *(cmdline - 1))
+			else if (*cmdline != prev_char)
 				(*sz)++;
 		}
 		else
 		{
-			if (minishell_iscmdsep(*(cmdline - 1)))
+			if (minishell_iscmdsep(prev_char))
 				(*sz)++;
 		}
 		(*sz)++;
+		prev_char = *cmdline;
 		cmdline++;
 	}
 	return (STATUS_SUCCESS);
-} 	
+}
 
 static bool	cmd_ignore_quotes(char **cmdline, char **spaced_cmdline)
 {
@@ -39,11 +43,12 @@ static bool	cmd_ignore_quotes(char **cmdline, char **spaced_cmdline)
 	return (true);
 }
 
-static t_status	cmd_spaced(char **cmdline, char **spaced_cmdline, bool cmdissep)
+static t_status	cmd_spaced(char **cmdline, char **spaced_cmdline,
+					char prev_char, bool cmdissep)
 {
 	if (!cmdissep)
 	{
-		if (minishell_iscmdsep(*((*cmdline) - 1)))
+		if (minishell_iscmdsep(prev_char))
 			*(*spaced_cmdline)++ = SPACE;
 		if (!cmd_ignore_quotes(cmdline, spaced_cmdline))
 			return (STATUS_SYNTAXERR);
@@ -51,12 +56,12 @@ static t_status	cmd_spaced(char **cmdline, char **spaced_cmdline, bool cmdissep)
 	}
 	else
 	{
-		if (minishell_iscmdsep(*((*cmdline) - 1))
+		if (minishell_iscmdsep(prev_char)
 			&& **cmdline == *((*cmdline) - 1))
 			*(*spaced_cmdline)++ = *(*cmdline)++;
 		else
 		{
-			if (*((*cmdline) - 1) != SPACE)
+			if ((prev_char) != SPACE)
 				*(*spaced_cmdline)++ = SPACE;
 			*(*spaced_cmdline)++ = *(*cmdline)++;
 		}
@@ -80,22 +85,25 @@ t_status	lexer_cmd_spaced(t_lexer *lexer)
 	t_status	status;
 	char		*cmdline;
 	char		*spaced_cmdline;
+	char		prev_char;
 
 	status = init_spaced(lexer);
 	if (status)
 		return (status);
 	cmdline = lexer->cmdline;
 	spaced_cmdline = lexer->spaced.spaced_cmdline;
+	prev_char = 0;
 	while (*cmdline)
 	{
 		if (!minishell_iscmdsep(*cmdline))
 		{
-			status = cmd_spaced(&cmdline, &spaced_cmdline, false);
+			status = cmd_spaced(&cmdline, &spaced_cmdline, prev_char, false);
 			if (status)
 				return (status);
 		}
 		else
-			cmd_spaced(&cmdline, &spaced_cmdline, true);
+			cmd_spaced(&cmdline, &spaced_cmdline, prev_char, true);
+		prev_char = *cmdline;
 	}
 	*spaced_cmdline = 0;
 	return (STATUS_SUCCESS);
